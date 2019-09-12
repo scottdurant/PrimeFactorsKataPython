3d242116abb0408123c4cd1ff2b3674e08f4fe39
scottdurant <scott.durant.16@cnu.edu>
2019-09-12 17:21:05 -0400

    test passes, refactored code

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -5,17 +5,17 @@ class PrimeFactors:
     @staticmethod
     def calculate_primes(self, given_number):
         result = []
+        divisor = 2
 
         if given_number == 1:
             return result
 
         while given_number != 1:
-            if given_number % 2 == 0:
-                result.append(2)
-                given_number = given_number / 2
-
-            if given_number == 3:
-                result.append(3)
-                given_number = given_number / 3
+            if given_number % divisor == 0:
+                result.append(divisor)
+                given_number = given_number / divisor
+            else:
+                result.append(given_number)
+                return result
 
         return result
